013bd37a98c37c9a529f3427405db0a3792d9b93
GhaziFs <gf.suruji@themwl.org>
2025-08-18 10:44:42 +0300

    7st work layout edit

--- src/components/layout/Page.jsx
+++ src/components/layout/Page.jsx
@@ -1,14 +1,36 @@
 import Navbar from "./NavBar";
 import Footer from "./Footer";
+import { useEffect } from "react";
 
 export default function Page({ children }) {
+  useEffect(() => {
+    // يتأكد أن الـ body ما فيه كلاس يقفل التمرير
+    document.body.classList.remove("overflow-hidden", "fixed");
+  }, []);
   return (
-    <div className="flex flex-col min-h-screen w-screen font-imb bg-white text-gray-900">
+    <div
+      className="
+        flex flex-col
+        min-h-[100svh]      
+        overflow-x-clip     
+        bg-white text-gray-900
+        font-imb
+        relative
+      "
+    >
       {/* Navbar */}
       <Navbar />
 
       {/* Main Content */}
-      <main className="flex-grow w-full">{children}</main>
+      <main
+        className="
+          flex-1            /* بدل flex-grow: مساحة التمرير الرئيسية */
+          min-h-0           /* مهم داخل flex لمنع قفل سكّرة داخلي */
+          w-full
+        "
+      >
+        {children}
+      </main>
 
       {/* Footer */}
       <Footer />
