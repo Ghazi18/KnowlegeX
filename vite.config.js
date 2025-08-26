e4838cfe9ab87ba68645ca20ba603c2a15a4d165
GhaziFs <gf.suruji@themwl.org>
2025-08-26 12:12:53 +0300

    brows - btn erorrs11

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,13 @@
+import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import legacy from '@vitejs/plugin-legacy'
+import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    legacy({
-      // استهدف متصفحات أقدم (عدّل بحسب جمهورك)
-      targets: [
-        'defaults',
-        'not IE 11',
-        'iOS >= 10',
-        'Android >= 5'
-      ],
-      // يضيف polyfills شائعة تلقائياً عند الحاجة
-      modernPolyfills: true, // Vite 5+
-      // لو تستخدم Vite أقدم:
-      // polyfills: ['es.symbol', 'es.promise', 'es.array.iterator', 'es.object.assign', 'es.promise.finally', 'es/map', 'es/set', 'es.array.includes', 'es.object.from-entries', 'es.string.includes', 'web.dom-collections.iterator']
-    })
-  ]
-})
+   build: {
+    target: 'es2019'
+  },
+  plugins: [react(), tailwindcss()],
+});
+
+
